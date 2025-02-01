8fc7f3b06b009f35f86746d9e36bc36731fc155b
Atharv1104 <atharvpore2004@gmail.com>
2025-02-01 22:29:24 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express =require("express");
+const app = express();
+const mongoose=require("mongoose");
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const path=require("path")
+const methodOverride=require("method-override");
+const ejsMate= require("ejs-mate");
+const Listing= require("./Models/listing.js")
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"public")))
+
+
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+})
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+
+app.get("/",(req,res)=>{
+    res.send("Hi i am root");
+});
+
+// index Route
+app.get("/listings",async (req,res)=>{
+    
+     const allListings=await Listing.find({});
+     res.render("./listings/index.ejs",{allListings})
+    });
+
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("./listings/new.ejs");
+    })
+
+//Read Route
+app.get("/listing/:id",async (req,res)=>{
+    let {id}=req.params;
+    const listing= await Listing.findById(id);
+    res.render("./listings/show.ejs",{listing});
+})
+
+//create
+app.post("/listings",async(req,res)=>{
+    const newListing=new Listing (req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
+
+})
+    
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
+})
+//update
+ app.put("/listings/:id",async(req,res)=>{
+    let {id}= req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}/edit`);
+ }
+)
+
+//Delete
+app.delete("/listings/:id/delete",async (req,res)=>{
+    let {id}= req.params;
+   let delListing= await Listing.findByIdAndDelete(id);
+   console.log(delListing);
+   res.redirect("/listings");
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Error 404.....page not found")
+})
+
+app.listen(8080,()=>{
+    console.log("App is listening at port 8080");
+});
